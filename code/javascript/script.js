325f578f541ec0e13a99e90f7f044ffeaa8e4e34
Michael Conroy <michaelshawnconroy@outlook.com>
2019-02-14 12:09:24 -0500

    added controls for 4 way stop, slow and go buuttons

--- code/javascript/script.js
+++ code/javascript/script.js
@@ -5,20 +5,46 @@ window.onload = function(){
 
     function illuminateRed() {
     clearLights();
-    document.getElementById('stopLight').style.backgroundColor = "red";
+    document.getElementById('stopLight-yn').style.backgroundColor = "red";
+    document.getElementById('stopLight-ys').style.backgroundColor = "red";
+
+    document.getElementById('goLight-xe').style.backgroundColor = "green";
+    document.getElementById('goLight-xw').style.backgroundColor = "green";
     }
 
     function illuminateYellow() {
     clearLights();
-    document.getElementById('slowLight').style.backgroundColor = "yellow";
+    document.getElementById('slowLight-yn').style.backgroundColor = "yellow";
+    document.getElementById('slowLight-ys').style.backgroundColor = "yellow";
+
+    document.getElementById('stopLight-xe').style.backgroundColor = "red";
+    document.getElementById('stopLight-xw').style.backgroundColor = "red";
     }
+
     function illuminateGreen() {
     clearLights();
-    document.getElementById('goLight').style.backgroundColor = "green";
+    document.getElementById('goLight-yn').style.backgroundColor = "green";
+    document.getElementById('goLight-ys').style.backgroundColor = "green";
+
+    document.getElementById('stopLight-xe').style.backgroundColor = "red";
+    document.getElementById('stopLight-xw').style.backgroundColor = "red";
     }
+
     function clearLights() {
-    document.getElementById('stopLight').style.backgroundColor = "black";
-    document.getElementById('slowLight').style.backgroundColor = "black";
-    document.getElementById('goLight').style.backgroundColor = "black";
+    document.getElementById('stopLight-yn').style.backgroundColor = "black";
+    document.getElementById('slowLight-yn').style.backgroundColor = "black";
+    document.getElementById('goLight-yn').style.backgroundColor = "black";
+
+    document.getElementById('stopLight-ys').style.backgroundColor = "black";
+    document.getElementById('slowLight-ys').style.backgroundColor = "black";
+    document.getElementById('goLight-ys').style.backgroundColor = "black";
+
+    document.getElementById('stopLight-xe').style.backgroundColor = "black";
+    document.getElementById('slowLight-xe').style.backgroundColor = "black";
+    document.getElementById('goLight-xe').style.backgroundColor = "black";
+
+    document.getElementById('stopLight-xw').style.backgroundColor = "black";
+    document.getElementById('slowLight-xw').style.backgroundColor = "black";
+    document.getElementById('goLight-xw').style.backgroundColor = "black";
     }
 }
