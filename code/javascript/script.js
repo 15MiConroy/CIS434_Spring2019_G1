db2de0ed5a81a5932faf69bf9594c8fd344ea98b
15MiConroy <michaelshawnconroy@outlook.com>
2019-03-05 17:17:02 -0500

    replaced original code with p5 version

--- code/javascript/script.js
+++ code/javascript/script.js
@@ -1,71 +1,58 @@
-window.onload = function(){
-    document.getElementById('stopButton').onclick = stop;
-    document.getElementById('goButton').onclick= go;
+var h = 480;
+var w = 640;
+var x = w;
+var y = h;
+var xSpeed;
+var ySpeed;
+var colorR = 200;
+var colorG = 0;
+var colorB = 0;
+//need an array of x initial positions
+var xPosition = [0, 265, 370, w]; 
+//need an array of y initial positions
+var yPosition = [270, 0, h, 170];
 
-    function stop(){
-        illuminateYellow();
-        setTimeout(illuminateRed,5000);
+function setup() {
+    createCanvas(w, h);
+    background(00,66,45);
+  }
+  
+  function draw() {
+    //x street
+    fill(51);
+    rect(-1,h/4,w+1,200);
+    //y streen
+    fill(51);
+    rect(w/3,-1,200,h);
+    fill(255);
+    rect(205, 220, 10, 100);
+    fill(255);
+    rect(413, 120, 10, 100);
+    fill(255);
+    rect(215, 110, 100, 10);
+    fill(255);
+    rect(315, 320, 100, 10);
+    fill(255, 204, 0);
+    rect(315, 320, 10, 160);
+    fill(255, 204, 0);
+    rect(315, -40, 10, 160);
+    fill(255, 204, 0);
+    rect(412, 210, 230, 10);
+    fill(255, 204, 0);
+    rect(-15, 210, 230, 10);
+    fill(colorR, colorG, colorB);
+    circle(x, 170, 10);
+    //    car();
+    x--;
+  }
+/*
+  class Car {
+    constructor() {
+        this.x = ; //one of 4 x position o
+        this.y = ; //one of 4 y position options
+        fill(colorR, colorG, colorB);
+        circle(0, y, 10);
+        var isMoving = true;
     }
-
-    function go(){
-        if(document.getElementById('stopLight-yn').style.backgroundColor == "red"){
-            illuminateYellow2();
-            setTimeout(illuminateGreen,5000);
-        }
-        else if(document.getElementById('stopLight-yn').style.backgroundColor == "black"){
-            illuminateGreen();
-        }
-    }
-
-    function illuminateRed() {
-    clearLights();
-    document.getElementById('stopLight-yn').style.backgroundColor = "red";
-    document.getElementById('stopLight-ys').style.backgroundColor = "red";
-    document.getElementById('goLight-xe').style.backgroundColor = "green";
-    document.getElementById('goLight-xw').style.backgroundColor = "green";
-    }
-
-    function illuminateYellow() {
-    clearLights();
-    document.getElementById('slowLight-yn').style.backgroundColor = "yellow";
-    document.getElementById('slowLight-ys').style.backgroundColor = "yellow";
-
-    document.getElementById('stopLight-xe').style.backgroundColor = "red";
-    document.getElementById('stopLight-xw').style.backgroundColor = "red";
-    }
-        function illuminateYellow2() {
-    clearLights();
-    document.getElementById('slowLight-xe').style.backgroundColor = "yellow";
-    document.getElementById('slowLight-xw').style.backgroundColor = "yellow";
-
-    document.getElementById('stopLight-yn').style.backgroundColor = "red";
-    document.getElementById('stopLight-ys').style.backgroundColor = "red";
-    }
-
-    function illuminateGreen() {
-    clearLights();
-    document.getElementById('goLight-yn').style.backgroundColor = "green";
-    document.getElementById('goLight-ys').style.backgroundColor = "green";
-
-    document.getElementById('stopLight-xe').style.backgroundColor = "red";
-    document.getElementById('stopLight-xw').style.backgroundColor = "red";
-    }
-
-    function clearLights() {
-    document.getElementById('stopLight-yn').style.backgroundColor = "black";
-    document.getElementById('slowLight-yn').style.backgroundColor = "black";
-    document.getElementById('goLight-yn').style.backgroundColor = "black";
-
-    document.getElementById('stopLight-ys').style.backgroundColor = "black";
-    document.getElementById('slowLight-ys').style.backgroundColor = "black";
-    document.getElementById('goLight-ys').style.backgroundColor = "black";
-
-    document.getElementById('stopLight-xe').style.backgroundColor = "black";
-    document.getElementById('slowLight-xe').style.backgroundColor = "black";
-    document.getElementById('goLight-xe').style.backgroundColor = "black";
-
-    document.getElementById('stopLight-xw').style.backgroundColor = "black";
-    document.getElementById('slowLight-xw').style.backgroundColor = "black";
-    document.getElementById('goLight-xw').style.backgroundColor = "black";
-    }
-}
+  }
+  */
