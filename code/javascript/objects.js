80c7e56c854e0b932c769ede3898b9de1a6e1a03
alexdarling <aldarling@hotmail.com>
2019-03-15 22:54:04 -0400

    Replacing cars.js with objects.js
    objects.js will include the Car and Lane objects

---
+++ code/javascript/objects.js
@@ -0,0 +1,45 @@
+class Car {
+    constructor(initX, initY, color, lane) {
+        this._x = initX;
+        this._y = initY;
+        this._color = color;    
+        this._moving = false;
+        this._lane = lane;
+    }
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y
+    }
+    get lane() {
+        return this._lane;
+    }
+    get color() {
+        return this._color;
+    }
+    get moving() {
+        return this._moving;
+    }
+    set x(newX) {
+        this._x = newX;
+    }
+    set y(newY) {
+        this._y = newY;
+    }
+    set color(color) {
+        this._color = color;
+    }
+    isMoving() {
+        return this._moving;
+    }
+    isNotMoving() {
+        return !this._moving;
+    }
+    move() {
+        this._moving = true;
+    }
+    stop() {
+        this._moving = false;
+    }
+}
