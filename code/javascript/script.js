823b8a9f8b7fa2d3c75debee6d865678b369745a
alexdarling <aldarling@hotmail.com>
2019-03-20 14:07:34 -0400

    Returning p5 draw() to normal

--- code/javascript/script.js
+++ code/javascript/script.js
@@ -29,31 +29,31 @@ function setup() {
     background(00,66,45);
 }
 
-// function draw() {
-//     //x street
-//     fill(51);
-//     rect(-1,h/4,w+1,200);
-//     //y streen
-//     fill(51);
-//     rect(w/3,-1,200,h);
-//     fill(255);
-//     rect(205, 220, 10, 100);
-//     fill(255);
-//     rect(413, 120, 10, 100);
-//     fill(255);
-//     rect(215, 110, 100, 10);
-//     fill(255);
-//     rect(315, 320, 100, 10);
-//     fill(255, 204, 0);
-//     rect(315, 320, 10, 160);
-//     fill(255, 204, 0);
-//     rect(315, -40, 10, 160);
-//     fill(255, 204, 0);
-//     rect(412, 210, 230, 10);
-//     fill(255, 204, 0);
-//     rect(-15, 210, 230, 10);
-//     fill(colorR, colorG, colorB);
-//     circle(x, 170, 10);
-//     //    car();
-//     x--;
-//   }
+function draw() {
+    //x street
+    fill(51);
+    rect(-1,h/4,w+1,200);
+    //y streen
+    fill(51);
+    rect(w/3,-1,200,h);
+    fill(255);
+    rect(205, 220, 10, 100);
+    fill(255);
+    rect(413, 120, 10, 100);
+    fill(255);
+    rect(215, 110, 100, 10);
+    fill(255);
+    rect(315, 320, 100, 10);
+    fill(255, 204, 0);
+    rect(315, 320, 10, 160);
+    fill(255, 204, 0);
+    rect(315, -40, 10, 160);
+    fill(255, 204, 0);
+    rect(412, 210, 230, 10);
+    fill(255, 204, 0);
+    rect(-15, 210, 230, 10);
+    fill(colorR, colorG, colorB);
+    circle(x, 170, 10);
+    //    car();
+    x--;
+}
