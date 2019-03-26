3e3c652b2a68c4e7e467b2560af0a87bb467b692
15MiConroy <michaelshawnconroy@outlook.com>
2019-03-26 17:15:49 -0400

    cars are controlled by lane light color

--- code/javascript/functions.js
+++ code/javascript/functions.js
@@ -9,8 +9,23 @@
     function updateCars() {
     if(car2.lane.light == "G")
     {
-        car2._x = car2._x + car2.xSpeed;
-        car2._y = car2._y + car2.ySpeed;
+        car2._x = car2._x + car2.xSpeed/2;
+        car2._y = car2._y + car2.ySpeed/2;
+    }
+    if(car1.lane.light == "G")
+    {
+        car1._x = car1._x + car1.xSpeed/2;
+        car1._y = car1._y + car1.ySpeed/2;
+    }
+    if(car3.lane.light == "G")
+    {
+        car3._x = car3._x + car3.xSpeed/2;
+        car3._y = car3._y + car3.ySpeed/2;
+    }
+    if(car4.lane.light == "G")
+    {
+        car4._x = car4._x + car4.xSpeed/2;
+        car4._y = car4._y + car4.ySpeed/2;
     }
   }
   
