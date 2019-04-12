144246641d5419df79bf110506c7b371636aaf40
alexdarling <aldarling@hotmail.com>
2019-04-11 20:15:56 -0400

    Moving lightControl definition to setup
    This seems to solve a bug that I can't identify

--- code/javascript/script.js
+++ code/javascript/script.js
@@ -6,12 +6,11 @@ String.prototype.replaceAt=function(index, replacement) {
 }
 
 /* Create the 4 Lanes */
+let lightControl;
 let laneN = new Lane("north", 275, "y",  1, 110, 265, 0,   0);
 let laneE = new Lane("east",  300, "x", -1, 413, w,   170, 1);
-let laneS = new Lane("south", 325,  "y", -1, 320, 370, h,   2);
+let laneS = new Lane("south", 325, "y", -1, 320, 370, h,   2);
 let laneW = new Lane("west",  350, "x",  1, 205, 0,   270, 3);
-
-let lightControl = new LightControl(laneN, laneE, laneS, laneW);
 
 let x = 0;
 
@@ -30,9 +29,8 @@ function stopSimulation() {
     noLoop();
 }
 
-
-
 function setup() {
+    lightControl = new LightControl(laneN, laneE, laneS, laneW);
     createCanvas(w, h);
     background(00,66,45);
     noLoop();
